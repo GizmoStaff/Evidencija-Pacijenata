50ea05ddc48f4e79dd6a313069d2557ae1d2c150
Marina <48516338+GizmoStaff@users.noreply.github.com>
2022-01-07 13:25:20 +0100

    Add files via upload

---
+++ EvidencijaPacijenata.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evidencijapacijenata;
+
+/**
+ *
+ * @author Marina Šarar
+ */
+public class EvidencijaPacijenata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         
+        PacijentView pView=new PacijentView();
+       
+        
+        Pacijent pModel= new Pacijent();
+      
+        PacijentController ltController=new PacijentController(pView,pModel);
+        
+        pView.setVisible(true);
+        
+        
+    }
+    
+}
